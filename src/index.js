6e312569c6bc4d64ccc6786747f78dead15bf442
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-01-29 19:36:10 +0200

    сделал модалку

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import css from "./style.css";
 import refs from './js/refs'
 import api from './js/api'
 import observer from './js/observer'
+import * as basicLightbox from "basiclightbox";
+import 'basiclightbox/dist/basicLightbox.min.css'
+
 
 console.log(api);
 
@@ -13,8 +16,19 @@ function searchFormSubmitHandler(event) {
   api.query = inputValue.query.value;
   observer()
   refs.form.reset()
-
 }
+
+refs.galleryList.addEventListener("click", (event) => {
+  if (event.target.nodeName === "IMG") {
+    let modalSrc = event.target.dataset.src;
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+      <img class'light-box-img' src="${modalSrc}" alt="">
+    </div>`);
+    instance.show();
+  }
+});
 
 // const observeBox = refs.observeBox
 
